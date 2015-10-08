c443793c0225eab080075986f6de5c995f139192
lucberger <luc.berger@gmail.com>
2015-10-08 05:28:52 +0200

    Create dblogin.php

---
+++ dblogin.php
@@ -0,0 +1,13 @@
+<?php
+$LocCSV ='/var/www/html/csv/extraits_ING';
+$LocCSVBNP='/var/www/html/csv/extraits_BNP';///var/www/html/extraits/extraits_BNP
+$host = "localhost";
+$user = "theuser";
+$pass = "thepwd";
+$db   = "Extraits";
+
+$Def_FrCpte= "thedefaultaccount";
+$Def_ToCpte="%";
+
+echo "Les dates sont Ann&eacute;e-Mois-Jour (AAAA-MM-DD) !<br>";
+?>
